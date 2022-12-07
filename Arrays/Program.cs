cb02597b3de0f0bf086ed217d83ccd4a662934e8
Uriel975 <71081303+Uriel975@users.noreply.github.com>
2022-12-06 23:50:58 -0600

    Nuevos aprendizajes

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,27 +6,39 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            ///24.While 2
-            int numero, numeroTriple;
-            string numeroCadena;
+            
 
-            Console.WriteLine("Ingrese un numero");
-            numeroCadena = Console.ReadLine();
-            numero = int.Parse(numeroCadena);
-            numeroTriple = numero * 3;
-            Console.WriteLine("El numero Triple es: " + numeroTriple);
-            //numero es diferente de 0
-            while (numero != 0)
+            for (int i = 1; i <= 100; i++)
             {
-                Console.WriteLine("Ingrese otro numero");
-                numeroCadena = Console.ReadLine();
-                numero = int.Parse(numeroCadena);
-                numeroTriple = numero * 3;
-                Console.WriteLine("El numero Triple es: " + numeroTriple);
+                if( (i%3)== 0 && (i % 5) == 0)
+                {
+                    Console.WriteLine($"{i}");
+                    //Console.WriteLine("TresCinco");
+
+                }
+                else if ((i % 3)==0 )
+                {
+                    Console.WriteLine($"{i}");
+                    //Console.WriteLine("TRES");
+                }
+                else if ((i % 5) == 0)
+                {
+                    Console.WriteLine($"{i}");
+                    //Console.WriteLine("CINCO");
+                }
+                else
+                {
+                    Console.WriteLine( $"{i}");
+                }
 
             }
-            //cierre
-            Console.WriteLine("Finalizo");
+            
+            //string[] nombres = {"Jose", "Daniel", "Miguel"};
+
+            ////Indice
+            //Console.WriteLine("El Nombre es: " + nombres[2]);
+
+            //Console.ReadLine();
 
         }
     }

1baacfa877ea1778878ebc4c1247aed20de5d22b
Uriel975 <71081303+Uriel975@users.noreply.github.com>
2022-12-25 22:50:48 -0600

    Navidad

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,14 +6,63 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+
+            /*Formas de definir un Array y Matrices*/
+            int[] Notas = new int[5];
+            Notas[0] = 10;
+            Notas[1] = 20;
+            Notas[2] = 30;
+            Notas[3] = 40;
+            Notas[4] = 50;
+            foreach (int Lista in Notas)
+            {
+                Console.WriteLine(Lista);
+            }
+
+
+
+
+            //int[] notas = { 10, 11, 12, 15, 21 };
+            //foreach(int not in notas)
+            //{
+            //    Console.WriteLine(not);
+            //}
+
+            //Video 32 ( Recorrer todo el array)
+            //string[] nombres = { "Luis", "Manuel", "Eduardo" };
+
+            /*Forma 1*/
+            //int nelementos = nombres.Length;
+            //for (int i = 0; i < nelementos; i++)
+            //{
+            //    Console.WriteLine(nombres[i]);
+            //}
+
+            /*Forma 2*/
+            //foreach(string nom in nombres)
+            //{
+            //    Console.WriteLine(nom);
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
             //===Array sirve para almacenar varios valores
             //==Con enteros
-            int[] notas = { 15, 12, 4, 7, 8, 9 };
-            Console.WriteLine("total de notas " + notas.Length);
-            Console.WriteLine("primera nota " + notas[1]);
+            //int[] notas = { 15, 12, 4, 7, 8, 9 };
+            //Console.WriteLine("total de notas " + notas.Length);
+            //Console.WriteLine("primera nota " + notas[1]);
 
             //=== DINAMICO ===
-            Console.WriteLine("La ultima nota: " + notas[notas.Length-1]);
+            //Console.WriteLine("La ultima nota: " + notas[notas.Length-1]);
 
             //=== ESTATICO ===
             //Console.WriteLine("Ultima nota es " + notas[5]);
